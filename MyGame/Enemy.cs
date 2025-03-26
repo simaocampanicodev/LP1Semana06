34949104bd5a4cc786e8bf5c85d4c6fdc3ed5ae5
txger. <simaocampanico.dev@gmail.com>
2025-03-26 14:40:04 +0000

    Adding GetName and TakeDamage to Enemy.cs from MyGame

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -4,8 +4,30 @@ namespace MyGame
 {
     public class Enemy
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
     }
-    
+
 }
-    
