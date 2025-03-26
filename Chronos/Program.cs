b8ef5f61aa03ebb1d98e4ef748689d351a919f56
txger. <simaocampanico.dev@gmail.com>
2025-03-26 14:45:08 +0000

    Implementing project Chronos with two cronos

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Chronos
 {
@@ -6,7 +8,18 @@ namespace Chronos
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(250);
+            crono1.Stop();
+            crono2.Stop();
+
+            Console.WriteLine($"Cronômetro 1: {crono1.ElapsedMilliseconds / 1000.0:F4} segundos");
+            Console.WriteLine($"Cronômetro 2: {crono2.ElapsedMilliseconds / 1000.0:F4} segundos");
         }
     }
 }
