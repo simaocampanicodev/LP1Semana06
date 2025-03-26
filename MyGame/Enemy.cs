fd671785b2db4c2c993dce9b893270344cd85a65
txger. <simaocampanico.dev@gmail.com>
2025-03-26 14:53:37 +0000

    Adding GetHealth, GetShield and SetName to the Enemy.cs in the project MyGame

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -7,16 +7,34 @@ namespace MyGame
         private string name;
         private float health;
         private float shield;
+
         public Enemy(string name)
         {
-            this.name = name;
+            SetName(name);
             health = 100;
             shield = 0;
         }
+
         public string GetName()
         {
             return name;
         }
+
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        public float GetShield()
+        {
+            return shield;
+        }
+
+        public void SetName(string newName)
+        {
+            name = newName.Substring(0, Math.Min(8, newName.Length));
+        }
+
         public void TakeDamage(float damage)
         {
             shield -= damage;
@@ -29,5 +47,4 @@ namespace MyGame
             }
         }
     }
-
 }
