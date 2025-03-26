89ded5ecd26307a66e717f40892f8b27b6c466cd
txger. <simaocampanico.dev@gmail.com>
2025-03-26 14:33:38 +0000

    Creating MyGame file and adding it to the solution

---
+++ MyGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGame
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
